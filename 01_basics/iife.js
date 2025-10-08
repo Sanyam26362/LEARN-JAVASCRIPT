64858b569148fbbd8de2e0a9454e11acdef352a6
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-10-08 12:41:51 +0530

    lessgoo

---
+++ 01_basics/iife.js
@@ -0,0 +1,7 @@
+//Immediately Invoked Function Expression
+// (function chai(){
+//     console.log(`DB CONNECTED`);
+// })();
+((name)=> {
+    console.log(`DB CONNECTED TO ${name}`);
+})('sanyam');
